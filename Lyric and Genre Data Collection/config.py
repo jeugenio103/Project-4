d6dd5ac9bcedbeae7dffae886a1fc7d3bc59b165
Master-Leo <emillio3198@gmail.com>
2022-12-25 21:27:02 -0800

    'updated config file & added csv's'

--- Lyric and Genre Data Collection/config.py
+++ Lyric and Genre Data Collection/config.py
@@ -1,1 +1,4 @@
 access = "kCLHaNOlkuuxjwjIP8lHD5wmqi4wPoCDgqCmFGv4xiR9dHqBnMTOkbMXS_6CPrKN"
+
+client_id = '46f5d2f533c84b87b65d2d65da27adc4'
+client_secret = 'f1acbf7a089c48eba9ad687ef198028f'
---
+++ Lyric and Genre Data Collection/risingArtistDict.py
@@ -0,0 +1,41 @@
+
+
+artists = ['This Is Joji','This Is NIKI','This Is Rich Brian','This Is BIBI','This Is Jackson Wang','This Is CHUNG HA','This Is Stephanie Poetri','This Is Higher Brothers','This Is Mili','This Is Guapdad 4000']
+
+artist = ['Joji','NIKI','Rich Brian','BIBI','Jackson Wang','CHUNG HA','Stephanie Poetri','Higher Brothers','Mili','Guapdad 4000']
+
+all_artist = ["Atarashii Gakko", 
+"BIBI", 
+"CHUNG HA",
+"Dumbfoundead",
+"Guapdad 4000",
+"Higher Brothers",
+"Jackson Wang",
+"Joji",
+"Keith Ape",
+"Lexie Liu",
+"Mili",
+"Niki", 
+"Rich Brian",
+"Seori",
+"Stephanie Poetri",
+"Warren Hue",
+"Ylona Garcia"]
+
+playlist_arts = ["This Is Atarashii Gakko", 
+"This Is BIBI", 
+"This Is CHUNG HA",
+"This Is Dumbfoundead",
+"This Is Guapdad 4000",
+"This Is Higher Brothers",
+"This Is Jackson Wang",
+"This Is Joji",
+"This Is Keith Ape",
+"This Is Lexie Liu",
+"This Is Mili",
+"This Is NIKI", 
+"This Is Rich Brian",
+"This Is Seori",
+"This Is Stephanie Poetri",
+"This Is Warren Hue",
+"This Is Ylona Garcia"]
