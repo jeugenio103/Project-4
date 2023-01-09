fcc78fed8d547b639a2cbe10cd15f86ecc348b5b
Master-Leo <emillio3198@gmail.com>
2023-01-09 13:22:08 -0800

    'updated ETL + flask format'

--- application.py
+++ application.py
@@ -22,6 +22,24 @@ import sys
 client_credentials_manager = SpotifyClientCredentials(client_id,client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
+#Loading CSV File 
+file_path = Path("./Test-Data/equalized_df.csv")
+df = pd.read_csv(file_path)
+df=df.drop(columns=["Unnamed: 0","track_id","track","artist","genre","duration_ms"])
+
+scaler = StandardScaler()
+scaled_data = scaler.fit_transform(df[["acousticness","energy","liveness","loudness","speechiness","valence","tempo","time_signature","danceability","key","instrumentalness","mode"]])
+
+X = df.drop("primary_genre", axis=1)
+y = df["primary_genre"].values
+
+label_encoder = LabelEncoder()
+y = label_encoder.fit_transform(y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, stratify=y, random_state=42)
+
+scaler = StandardScaler().fit(X_train)
+X_train_scaled = scaler.transform(X_train)
+X_test_scaled = scaler.transform(X_test)
 
 
 application = Flask(__name__,template_folder='templates',static_url_path='/static')
@@ -51,25 +69,6 @@ def predict():
     if request.method == 'POST':
         # Get the track name from the form data
         track_name = request.form.get('input')
-        from sklearn.preprocessing import StandardScaler 
-        from pathlib import Path
-        #Loading CSV File 
-        file_path = Path("./Test-Data/equalized_df.csv")
-        df = pd.read_csv(file_path)
-        df=df.drop(columns=["Unnamed: 0","track_id","track","artist","genre","duration_ms"])
-        from sklearn.preprocessing import StandardScaler 
-        scaler = StandardScaler()
-        scaled_data = scaler.fit_transform(df[["acousticness","energy","liveness","loudness","speechiness","valence","tempo","time_signature","danceability","key","instrumentalness","mode"]])
-        X = df.drop("primary_genre", axis=1)
-        y = df["primary_genre"].values
-        from sklearn.preprocessing import LabelEncoder
-        label_encoder = LabelEncoder()
-        y = label_encoder.fit_transform(y)
-        from sklearn.model_selection import train_test_split
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, stratify=y, random_state=42)
-        scaler = StandardScaler().fit(X_train)
-        X_train_scaled = scaler.transform(X_train)
-        X_test_scaled = scaler.transform(X_test)
 
         if track_name:
             # Initialize the df2 variable
@@ -83,7 +82,6 @@ def predict():
 
             # Create a Pandas DataFrame with one row
             df2 = pd.DataFrame([features])
-
             df2 = df2.loc[:,["key","mode","time_signature","acousticness","danceability","energy","instrumentalness","liveness","loudness","speechiness","valence","tempo"]]
 
             pickled_model = pickle.load(open('./Lyric and Genre Data Collection/Machine Learning Models/svm_model.pkl', 'rb'))
